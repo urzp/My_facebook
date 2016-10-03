d4505a33389e7bb0feceb4122a3bb3063c60f7d6
urzp <urzp002@mail.ru>
2016-10-03 22:21:07 +0300

    add test

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,7 @@ module MyFacebook
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
-    config.generator do |g|
+    config.generators do |g|
       g.template_engine :haml
       g.test_framework :rspec, fixtures: true, views: false
       g.fixture_replacement :factory_girl, dir: "spec/factories"
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+
+  describe "test rspec" do
+
+    it "should return 4" do
+        expect(2+2).to eq(4)
+    end
+
+  end
+
+  describe "User relationships" do
+
+    it "shoud not exeption" do
+      user1 = User.new
+      expect(user1.wish_frends).to eq([])
+    end
+
+    it "shoud not exeption" do
+      user = User.new
+      expect(user.inv_frends).to eq([])
+    end
+
+    it "shoud return inv_frends" do
+      user1 = User.new(id:1)
+      user2 = User.new(id:2)
+      user1.wish_frends << user2
+      user1.save
+      user2.save
+      expect(user2.inv_frends).to eq([])
+    end
+
+  end
